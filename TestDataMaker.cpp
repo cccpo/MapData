5e4627ee69446da57dcf8730b96b0cabe1a28a42
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-08-05 17:30:55 +0900

    TestDataMaker:経路データを乱数生成するように実装

--- TestDataMaker.cpp
+++ TestDataMaker.cpp
@@ -1,9 +1,42 @@
 #include "TestDataMaker.h"
 
+#include <fstream>
 TestDataMaker::TestDataMaker() {
-
+	GenerateData();
 }
 
 TestDataMaker::~TestDataMaker() {
+	//delete &mData;
+}
+
+void TestDataMaker ::GenerateData() {
+	random_device rnd;
+	mt19937 mt(rnd());
+	uniform_int_distribution<int> type(0, 9);//0`9‚Ì”ÍˆÍ‚Å—””­¶
+	uniform_int_distribution<int> second_number(2, 4);//2`4‚Ì”ÍˆÍ‚Å—””­¶
+	
+	ofstream test_file("Test.txt");
+
+	if (test_file.fail()) {
+		/*return */
+		cout << "Error:Can't read text" << endl;
+	}
+
+	test_file << "PlayerID:ght" << endl;
+	test_file << "PlayerCourse:";
+	for (int i = 0; i < 10; ++i) {
+		if (i == 0) {
+			mData[i] = type(mt);
+		}else {
+			mData[i] = mData[i - 1] + second_number(mt) - 3;
+
+		}
+
+		test_file << mData[i] << ",";
+	}
+	
+	test_file.close();
+
+
 
 }
--- TestDataMaker.h
+++ TestDataMaker.h
@@ -1,8 +1,21 @@
 #pragma once
+#include <random>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <array>
+
+using namespace std;
+
 class TestDataMaker
 {
 public:
 	TestDataMaker();
 	~TestDataMaker();
+
+	void GenerateData();
+protected:
+	string mPlayerId;//PlayerID
+	array<int, 10> mData;
 };
 
