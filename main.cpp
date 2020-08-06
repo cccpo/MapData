ae75bd82fd022a787103f1139ca141c55a56a6a9
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-08-06 12:25:57 +0900

    main:テストデータの変更

--- main.cpp
+++ main.cpp
@@ -11,21 +11,14 @@ int main() {
 	DataInput data_input;
 	TestDataMaker test_data_maker;
 
-	///*cout << "Test Case" << endl;*/
-	//array<int,10> player_route=data_input.GetPlayerRoute();
-	//
-	//for (int i = 0; i < 10; i++) {
-	//	cout << player_route[i]<<",";
-	//}
+	cout << "Make Test Data" << endl;
+	
+	test_data_maker.GenerateData();
 
-	//cout << "Make Test Data" << endl;
-	//
-	//test_data_maker.GenerateData();
-
-	//cout << "SuccessSSss" << endl;
+	cout << "Success" << endl;
 
 	vector<string> ioVectorDatas;
-	data_input.GetFileData("test.txt", ioVectorDatas);
+	data_input.GetFileData("Test1.log", ioVectorDatas);
 
 	cout << ioVectorDatas.size() << endl;
 
