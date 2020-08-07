f5c581b46a195a8981e9919e92ccd688aee142c5
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-08-07 13:16:54 +0900

    DataToolクラス;文字列を分割する関数の追加

--- DataTool.cpp
+++ DataTool.cpp
@@ -7,3 +7,25 @@ DataTool::DataTool() {
 DataTool::~DataTool() {
 
 }
+
+vector<string> DataTool::Split(string inWord, char inDeleteWord) {
+    int first = 0;
+    int last = inWord.find_first_of(inDeleteWord);
+
+    vector<string> result;
+
+    while (first < inWord.size()) {
+        string subStr(inWord, first, last - first);
+
+        result.emplace_back(subStr);
+
+        first = last + 1;
+        last = inWord.find_first_of(inDeleteWord, first);
+
+        if (last == string::npos) {
+            last = inWord.size();
+        }
+    }
+
+    return result;
+}
--- DataTool.h
+++ DataTool.h
@@ -1,4 +1,10 @@
 #pragma once
+#include <string>
+#include <iostream>
+#include <filesystem>
+#include <vector>
+
+using namespace std;
 
 //今回のプログラムに必要と思われる機能を集約
 //今後適したクラスに関数を移植予定
@@ -7,6 +13,8 @@ class DataTool
 public:
 	DataTool();
 	~DataTool();
+
+	vector<string> split(string str, char del);
 
 };
 
