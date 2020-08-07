bbb079eeedd1a0ed5e5460f8b0623efe425eb89c
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-08-07 10:20:49 +0900

    main.cpp:時間計測に使用するクラスをsystem_clockからhigh_resolution_clockに変更

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include "DataInput.h"
 #include "TestDataMaker.h"
+#include "DataTool.h"
 
 
 using namespace std;
@@ -11,17 +12,19 @@ using namespace std;
 int main() {
 	DataInput data_input;
 	TestDataMaker test_data_maker;
+	DataTool data_tool;
 	
-	chrono::system_clock::time_point start, end;
+	//chrono::system_clock::time_point start, end;
+	chrono::high_resolution_clock::time_point start, end;
 
 	cout << "Make Test Data" << endl;
 	cout << "FileName" << endl;
 	
-	start = chrono::system_clock::now();
+	start = chrono::high_resolution_clock::now();
 
-	test_data_maker.GenerateData(1000);
+	test_data_maker.GenerateData(1000);//テストデータ生成
 
-	end = chrono::system_clock::now();
+	end = chrono::high_resolution_clock::now();
 
 	double time = static_cast<double>(chrono::duration_cast<chrono::microseconds>(end - start).count() / 1000.0);
 
