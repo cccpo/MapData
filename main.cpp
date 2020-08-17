1cadd3bb63958446ee9c16be26e3fe77abf8788b
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-08-17 13:11:59 +0900

    (問題発生)CSVのロードができなくなってしまった、この間にやったこととしてはVisual Studioのアップデートでなので早急に対応していく。

--- main.cpp
+++ main.cpp
@@ -20,27 +20,28 @@ int main() {
 	
 	chrono::high_resolution_clock::time_point start, end;
 
-	string file_path = data_tool.SetFilePath("test21SS.csv");
+	string file_name = "testdata110.csv";
+	string file_path = data_tool.SetFilePath("testdata110.csv");
 
 //テストデータ生成
-	//{
-	//	cout << "Make Test Data" << endl;
+	{
+		//cout << "Make Test Data" << endl;
 
-	//	start = chrono::high_resolution_clock::now();//計測開始
-	//	test_data_maker.GenerateTestDataCSV(file_name, 10000);
-	//	end = chrono::high_resolution_clock::now();//計測終了
+		//start = chrono::high_resolution_clock::now();//計測開始
+		//test_data_maker.GenerateTestDataCSV(file_name, 10000);
+		//end = chrono::high_resolution_clock::now();//計測終了
 
-	//	double time = static_cast<double>(chrono::duration_cast<chrono::microseconds>(end - start).count() / 1000.0);
-	//	cout << "Success" << endl;
-	//	cout << "Time:" << time << endl;
-	//}
+		//double time = static_cast<double>(chrono::duration_cast<chrono::microseconds>(end - start).count() / 1000.0);
+		//cout << "Success" << endl;
+		//cout << "Time:" << time << endl;
+	}
 //End
 
-	vector<string> data_list;
-	vector<string> data_list2;
-	vector<int> dala_si;
-	string tesu_a;
-	string player_name = "PlayerA";
+	//vector<string> data_list;
+	//vector<string> data_list2;
+	//vector<int> dala_si;
+	//string tesu_a;
+	//string player_name = "PlayerA";
 
 //テストデータ読み込み
 	//#TODO 同一フォルダ内のデータをリストに追加する
@@ -50,7 +51,7 @@ int main() {
 
 	cout << "Load Test Data" << endl;
 	start = chrono::high_resolution_clock::now();//計測開始
-	data_input.LoadCSVData(file_path);//CSVデータの読み込み
+	data_input.LoadCSVData(file_name);//CSVデータの読み込み
 	end = chrono::high_resolution_clock::now();//計測終了
 
 	double time = static_cast<double>(chrono::duration_cast<chrono::microseconds>(end - start).count() / 1000.0);
@@ -64,10 +65,11 @@ int main() {
 	//data_list = data_input.SearchPlayerData(player_name);
 	//cout << data_input.GetPlayIDData(1, 1);
 	//data_list = data_input.GetPlayerCourseData(1);
-	
-	//for (int i = 0; i < data_list.size(); i++)
+	//
+	//for (int i = 0; i < data_list.size(); i++) {
 	//	tesu_a = to_string(dala_si[i]);
 	//	cout << data_list.at(i) << endl;
+	//}
 
 	return 0;
 }
