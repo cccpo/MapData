8cd07b2fbd8fb6059ce2455282a64b1fd51e9f54
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-08-17 17:11:45 +0900

    DataSort.h:要素抽出関数の追加

--- DataSort.h
+++ DataSort.h
@@ -1,9 +1,24 @@
 #pragma once
-class DataSort
-{
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+
+class DataSort{
 public:
 	DataSort();
 	~DataSort();
+
+	vector<vector<string>> mSortVectorDates;//ÉfÅ[É^Çäiî[Ç∑ÇÈvector
+
+	void NumOfSame(vector<vector<string>> inKSA);
 protected:
+	//DataInput data_input;
 };
 
