131cd16671a64be585da870799bfb7018793ca6f
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-08-17 17:22:56 +0900

    Dataクラス;テストデータ生成実行関数を実装

--- Data.cpp
+++ Data.cpp
@@ -7,3 +7,11 @@ Data::Data() {
 Data::~Data() {
 
 }
+
+const void Data::GenerateTestData(string& inFileName) {
+	cout << "Make Test Data" << endl;
+
+	test_data_maker.GenerateTestDataCSV(inFileName, 10000);
+
+	cout << "Success" << endl;
+}
--- Data.h
+++ Data.h
@@ -1,9 +1,14 @@
 #pragma once
+#include <iostream>
+#include <array>
 
-#include "DataInput.h"
+//#include "DataInput.h"
 #include "TestDataMaker.h"
 #include "DataTool.h"
 #include "DataSort.h"
+
+using namespace std;
+
 class Data
 {
 public:
@@ -11,8 +16,10 @@ public:
 	Data();
 	~Data();
 
+	const void GenerateTestData(string& inFileName);
+
 protected:
-	DataInput data_input;
+	//DataInput data_input;
 	TestDataMaker test_data_maker;
 	DataTool data_tool;
 	DataSort data_sort;
