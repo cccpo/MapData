9de99d33b3d6da5b17d0b5b45613cb5b5907006b
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-08-21 14:56:23 +0900

    Data.cpp:メッセージの追加

--- Data.cpp
+++ Data.cpp
@@ -19,20 +19,22 @@ const void Data::GenerateTestData(string& inFileName) {
 
 //CSVデータ読取実行関数
 const void Data::LoadTestData(string& inFileName) {
-	cout << "Load Test Data" << endl;
+	cout << "Load Data..." << endl;
 
-	mVectorDates = data_input.LoadCSVData(inFileName);//CSVファイルからデータを抽出
+	mVectorDates = data_input.LoadCSVData(inFileName);//CSVファイルからデータを抽出し、メンバ変数に格納
+
+	cout << "Loading Completed" << endl;
 
 	mSortVectorDates = data_sort.DeleteKeyCategory(mVectorDates);//プレイヤーネームの削除
 	
+	cout << "Extract Data..." << endl;
+
 	mPriorityDates = data_sort.ExtractData(mSortVectorDates);
-	//vector<vector<string>>().swap(mSortVectorDates);
-	
+
+	cout << "Data Extraction Completed" << endl;
+
 	string output_file_name = "AfterData2.csv";
 	
 	data_tool.MakeResultData(mPriorityDates, output_file_name);
-	//vector<vector<string>>().swap(mPriorityDates);
 
-
-	cout << "Success" << endl;
 }
