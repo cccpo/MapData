38924b8dad9299f3233ad2d1eb6e025f02003c2b
cccpo <ciko18010@g.nihon-u.ac.jp>
2020-08-26 11:07:12 +0900

    DataTool.cpp:文字列を分割し、数値として返すSplitNumber(暫定)の追加

--- DataTool.cpp
+++ DataTool.cpp
@@ -54,6 +54,30 @@ const list<string> DataTool::SplitToList(string& inLine, char inDeleteWord) {
     return result;
 }
 
+//‘ÎÛ‚Ì•¶š—ñ‚©‚çw’è‚µ‚½•¶š‚²‚Æ‚É•ªŠ„
+const list<int> DataTool::SplitNumber(string& inLine, char inDeleteWord) {
+    unsigned __int32 line_first = 0;
+    unsigned __int32 line_last = inLine.find_first_of(inDeleteWord);
+
+    list<int> result;
+
+    while (line_first < inLine.size()) {
+        string subStr(inLine, line_first, line_last - line_first);
+
+        result.emplace_back(subStr);
+
+        line_first = line_last + 1;
+        line_last = inLine.find_first_of(inDeleteWord, line_first);
+
+        if (line_last == string::npos) {
+            line_last = inLine.size();
+        }
+    }
+
+    return result;
+}
+
+
 bool DataTool::GetFileNames(string inFolderPath, vector<string>& inFileName)
 {
     return true;
